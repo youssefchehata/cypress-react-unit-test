2b814f27e643b0a704db8fd6742fe76ad4981269
CHEHATA Youssef <youssef.chehata@softeam.fr>
2025-03-20 18:44:42 +0100

    Initial Cypress test: counter component

--- src/counter.cy.jsx
+++ src/counter.cy.jsx
@@ -3,20 +3,20 @@ import Counter from "./Counter";
 
 describe("<Counter />", () => {
   it("test intial value", () => {
-    // see: https://on.cypress.io/mounting-react
+    
     cy.mount(<Counter />);
     cy.get(".value").should("have.text", 0);
   });
 
   it("test increment value", () => {
-    // see: https://on.cypress.io/mounting-react
+ 
     cy.mount(<Counter />);
     cy.get(".inc__button").click();
     cy.get(".value").should("have.text", 1);
   });
 
   it("test decrement value", () => {
-    // see: https://on.cypress.io/mounting-react
+
     cy.mount(<Counter />);
     cy.get(".dec__button").click();
     cy.get(".value").should("have.text", -1);
