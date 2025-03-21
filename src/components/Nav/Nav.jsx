0f0a1290cd7967e0827a596ff777e08710b59171
CHEHATA Youssef <youssef.chehata@softeam.fr>
2025-03-21 17:15:01 +0100

    fix err

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,13 +4,7 @@ import React from "react";
 function Nav({ count }) {
   return (
     <Navbar fluid={true} rounded={true}>
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-      </Navbar.Brand>
+
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Link href="/navbars" active={true}>
