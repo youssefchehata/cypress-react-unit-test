2538e9df60848d728a8d4e8d545e4d310162897e
CHEHATA Youssef <youssef.chehata@softeam.fr>
2025-03-21 16:56:01 +0100

    feat(ui): implement list of cards component
    - Created a new CardList component to display multiple cards
    - Added responsive grid layout for card arrangement
    - Implemented card rendering from an array of data
    - Ensured proper spacing and alignment between cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
-import './App.css'
-import Counter from "./Counter.jsx"
+import React from "react";
+import "./App.css";
+import CardGroup from "./components/Card/CardGroup";
+import Nav from "./components/Nav/Nav";
+import SearchForm from "./components/SearchForm/SearchForm";
+import { $DATA } from "./data/FakeData";
+// import Counter from './Counter';
 function App() {
-
-
   return (
-    <>
-    <Counter/>
-    </>
-  )
+    <div className="App max-w-5xl mx-auto">
+      {/* <Counter/> */}
+      <div className="nav__app">
+        <Nav count={10} />
+      </div>
+      <div className="search__app">
+        <SearchForm />
+      </div>
+      <div className="card__app mt-4">
+        <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-x-2 gap-y-2">
+          {$DATA.map(({ title, image, price, rating }) => {
+            return (
+              <CardGroup
+                title={title}
+                image={image}
+                price={price}
+                rating={rating}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Form from "./pages/Form";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/form",
+    element: <Form />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
